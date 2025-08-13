056b2f8b40a00f0cf27a075b3c08faeceafd18c0
uroobaCodes <urooba.codes@gmail.com>
2025-08-13 13:57:06 +0500

    trying to fix prisma error

--- src/middleware.ts
+++ src/middleware.ts
@@ -29,21 +29,36 @@ export async function updateSession(request: NextRequest) {
   //   },
   // );
 
+  // const supabase = createServerClient(
+  //   process.env.SUPABASE_URL!,
+  //   process.env.SUPABASE_ANON_KEY!,
+  //   {
+  //     cookies: {
+  //       getAll() {
+  //         return request.cookies.getAll();
+  //       },
+  //       setAll(cookiesToSet) {
+  //         cookiesToSet.forEach(({ name, value }) =>
+  //           request.cookies.set(name, value),
+  //         );
+  //         supabaseResponse = NextResponse.next({
+  //           request,
+  //         });
+  //         cookiesToSet.forEach(({ name, value, options }) =>
+  //           supabaseResponse.cookies.set(name, value, options),
+  //         );
+  //       },
+  //     },
+  //   },
+  // );
+
   const supabase = createServerClient(
     process.env.SUPABASE_URL!,
     process.env.SUPABASE_ANON_KEY!,
     {
       cookies: {
-        getAll() {
-          return request.cookies.getAll();
-        },
-        setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value }) =>
-            request.cookies.set(name, value),
-          );
-          supabaseResponse = NextResponse.next({
-            request,
-          });
+        getAll: () => request.cookies.getAll(),
+        setAll: (cookiesToSet) => {
           cookiesToSet.forEach(({ name, value, options }) =>
             supabaseResponse.cookies.set(name, value, options),
           );
